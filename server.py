15d41ead6c094be686f42729e4a93a4155557a08
Marc <marc@localhost.localdomain>
2019-04-15 19:57:48 +0200

    Alive updated

--- server.py
+++ server.py
@@ -1,34 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim: set fileencoding=utf-8 :
-
-"""
-SYNOPSIS
-
-    echoudp-server.py [-h,--help] [--version] [-p,--port <port>, default=1234]
-
-DESCRIPTION
-
-    Creates a ECHO/UDP server, resending all received data back to sender.
-    Default port 1234
-
-
-EXAMPLES
-
-    echoudp-server.py  --port 4321
-
-AUTHOR
-
-    Carles Mateu <carlesm@carlesm.com>
-
-LICENSE
-
-    This script is published under the Gnu Public License GPL3+
-
-VERSION
-
-    0.0.1
-"""
+#!/usr/bin/env python 3
 
 import sys, os, traceback, optparse
 import time, datetime
@@ -37,14 +7,15 @@ from struct import *
 import os
 import signal
 from multiprocessing import Process, Value, Array, Manager
+from random import seed
+from random import randint
 
-
-__program__ = "echoudp-server"
+__program__ = "server"
 __version__ = '0.0.1'
-__author__ = 'Carles Mateu <carlesm@carlesm.com>'
-__copyright__ = 'Copyright (c) 2012  Carles Mateu '
+__author__ = 'Marc Perez>'
+__copyright__ = 'Copyright (c) 2019  Marc Perez '
 __license__ = 'GPL3+'
-__vcs_id__ = '$Id: echoudp-server.py 554 2012-05-06 08:07:51Z carlesm $'
+__vcs_id__ = '$Id: server.py 554 2019-04-15 18:07:51Z mpa19 $'
 
 
 class clients:
@@ -75,7 +46,7 @@ def leerConfig():
 
     while datos != "\n":
         datosClient = datos.split()
-        listaClientes.append(clients(datosClient[0],"",datosClient[1],"","DISCONNECTED"))
+        listaClientes.append(clients(datosClient[0],"        -",datosClient[1],"     -","DISCONNECTED"))
         datos = f.readline()
 
     f.close()
@@ -92,37 +63,43 @@ def leerConfig():
     f.close()
 
 
+def randomNumber():
+    global listaClientes
+    numInvalido = False
+
+    while(1):
+        value = randint(100000,999999)
+        for indexClient in range(len(listaClientes)):
+            if listaClientes[indexClient].numAl == str(value):
+                numInvalido = True
+        if numInvalido == False:
+            return str(value)
 
 def registro(indexClient, index):
     global adreca, datosServer, sock, data, datosClient, listaClientes
 
     data[index] = list(data[index])
     data[index][0] = 0x01
+    random = randomNumber()
+    a = pack('B7s13s7s50s',data[index][0], datosServer[0].encode('utf-8'),datosServer[1].encode('utf-8'),random.encode('utf-8'),datosServer[3].encode('utf-8'))
 
-    listaClientes[indexClient].numAl = "715642"
-
-    a = pack('B7s13s7s50s',data[index][0], datosServer[0].encode('utf-8'),datosServer[1].encode('utf-8'),listaClientes[indexClient].numAl.encode('utf-8'),datosServer[3].encode('utf-8'))
     cliente = listaClientes[indexClient]
     cliente.estat = "REGISTERED"
+    cliente.numAl = random
+    cliente.ip = adreca[index][0]
     listaClientes.pop(indexClient)
     listaClientes.insert(indexClient, cliente)
-    #listaClientes[indexClient].estat = "REGISTERED"
 
     sock.sendto(a,adreca[index])
-
-
 
 def entradaPaquet():
     global data, adreca, sock, data, listaClientes
 
     i = 0
     while(1):
-
-
         data1,adreca1 = sock.recvfrom(78)
         data.append(data1)
         adreca.append(adreca1)
-        #paquete = data
         p2 = Process(target=gestionarPaquet, args=(i,))
         p2.daemon = True
         p2.start()
@@ -131,23 +108,17 @@ def entradaPaquet():
         i += 1
 
 
-
 def gestionarPaquet(index):
     global data, adreca, sock, data, listaClientes
     data[index] = unpack('=B7s13s7s50s',data[index][:78])
     if data[index][0] == 0:
-
         correcto = comprobarEstado(0, index)
         if correcto != None:
             registro(correcto, index)
 
     elif data[index][0] == 16:
-
-
         correcto = comprobarEstado(1, index)
         if correcto != None:
-
-
             enviarAlive(correcto, index)
 
 
@@ -179,26 +150,49 @@ def comprobarEstado(tipo, index):
             print(listaClientes[indexClient].estat)
             print(listaClientes[indexClient].nom)
 
-
             if listaClientes[indexClient].estat == "REGISTERED":
                 clientsAlive.append(listaClientes[indexClient].nom)
                 timeAlive.append(6)
-                cliente = listaClientes[indexClient]
-                cliente.estat = "ALIVE"
-                listaClientes.pop(indexClient)
-                listaClientes.insert(indexClient, cliente)
+                if comprobarAlive(indexClient, index) == True:
+                    cliente = listaClientes[indexClient]
+                    cliente.estat = "ALIVE"
+                    listaClientes.pop(indexClient)
+                    listaClientes.insert(indexClient, cliente)
+                    return indexClient
+                else:
+                    return None
 
-                return indexClient
             elif listaClientes[indexClient].estat == "ALIVE":
-                indexAlive = clientsAlive.index(listaClientes[indexClient].nom)
-                timeAlive[indexAlive] = 6
-                return indexClient
+                if comprobarAlive(indexClient, index) == True:
+                    indexAlive = clientsAlive.index(listaClientes[indexClient].nom)
+                    timeAlive[indexAlive] = 6
+                    return indexClient
+                else:
+                    return None
             else:
+                #Enviar paquete de error "no registrado"
+                enviarPaqueteError(index, "Equip no autoritzat en el sistema", 0x13)
                 return None
     else:
         #Enviar paquete que no se encuentra registrado
         enviarPaqueteError(index, "Equip no autoritzat en el sistema", 0x03)
         return None
+
+def comprobarAlive(indexClient, index):
+    global listaClientes, data, adreca
+
+    nom = data[index][1].split(b'\0',1)[0]
+    mac = data[index][2].split(b'\0',1)[0]
+    numAl = data[index][3].split(b'\0',1)[0]
+    if nom.decode('utf-8') == listaClientes[indexClient].nom and \
+        mac.decode('utf-8') == listaClientes[indexClient].mac and \
+        str(adreca[index][0]) == listaClientes[indexClient].ip and \
+        numAl.decode('utf-8') == listaClientes[indexClient].numAl:
+        return True
+    else:
+        #Enviar paquete de error "no coincide la informacion"
+        enviarPaqueteError(index, "Error en dades de l'equip", 0x12)
+        return False
 
 def enviarPaqueteError(index, msg, tipus):
     global sock, data
@@ -209,14 +203,13 @@ def enviarPaqueteError(index, msg, tipus):
     data[index][2] = "000000000000"
     data[index][4] = msg
 
-
     a = pack('B7s13s7s50s',data[index][0], data[index][1].encode('utf-8'),data[index][2].encode('utf-8'),data[index][3],data[index][4].encode('utf-8'))
     sock.sendto(a,adreca[index])
-    print("enviado")
 
 def enviarAlive(destinatari, index):
     global adreca, datosServer, sock, data, datosClient, listaClientes
 
+    print("ENVIAR ALIVE")
     data[index] = list(data[index])
     data[index][0] = 0x11
 
@@ -231,8 +224,17 @@ def alives():
         for x, val in enumerate(timeAlive):
             timeAlive[x] -= 1
             if timeAlive[x] == 0:
+                for indexClient in range(len(listaClientes)):
+                    if listaClientes[indexClient].nom == clientsAlive[x]:
+                        cliente = listaClientes[indexClient]
+                        cliente.ip = "        -"
+                        cliente.numAl = "     -"
+                        cliente.estat = "DISCONNECTED"
+                        listaClientes.pop(indexClient)
+                        listaClientes.insert(indexClient, cliente)
                 timeAlive.pop(x)
                 clientsAlive.pop(x)
+                listaClientes
         time.sleep(1)
 
 
@@ -253,12 +255,8 @@ if __name__ == '__main__':
             timeAlive = manager.list()
             listaClientes = manager.list()
 
-
-
             leerConfig()
             setup()
-
-
 
             p = Process(target=alives, args=())
             p.daemon = True
@@ -269,24 +267,10 @@ if __name__ == '__main__':
 
             #crear otro daemon para controlar la entrada recvfrom
             p1 = Process(target=entradaPaquet)
-            #p1.daemon = True
             p1.processes = False
             p1.start()
-            #p1.join(1)
 
             print("HOLA")
-            #fn = sys.stdin.fileno()
-            #p3 = Process(target=mirarComando, args=(fn,))
-            #p3.daemon = True
-            #p3.start()
-            #p3.join(1)
-
-            #entradaPaquet()
-
-
-
-            #clientsAlive.append("Mario")
-            #timeAlive.append(10)
 
             while 1:
                 name = input()
@@ -294,8 +278,11 @@ if __name__ == '__main__':
                     p1.terminate()
                     sys.exit(0)
                 elif name == 'list':
-                    print(clientsAlive[0])
-
+                    print("-NOM-- ------IP------- -----MAC---- -ALEA- ----ESTAT---")
+                    for indexClient in range(len(listaClientes)):
+                        print(listaClientes[indexClient].nom,"      ", listaClientes[indexClient].ip, listaClientes[indexClient].mac, listaClientes[indexClient].numAl, listaClientes[indexClient].estat)
+                else:
+                    print("MSG.  =>  Comanda incorrecta")
 
 
 
